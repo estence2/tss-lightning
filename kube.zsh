0d4459557a1fdb3d3c856da8d31efbe3c39b29c6
estence2 <ellen.stence@iterable.com>
2022-10-13 20:36:04 -0600

    Pre-testing final script

--- kube.zsh
+++ kube.zsh
@@ -16,13 +16,13 @@ else
 fi
 read -p "
 1. campaign
-2. template
+2. template (use this option for proofs)
 3. journey
 4. list
 5. custom event
 6. user update
 Include the above id's? Enter corresponding number or press enter to skip: " id_type
-#if id not null
+#if type of id not null
 if [[ -n "$id_type" ]]; then
 #check id type and get the id
   #if campaign
@@ -36,22 +36,54 @@ if [[ -n "$id_type" ]]; then
   #if workflow
   elif [[ $id_type == 3 ]]; then
     read -p "Enter workflow id: " id
-    subquery="log:\"$id\""
+    #if id not null
+    if [[ -n "$id" ]]; then
+      subquery="log:\"$id\""
+    fi
+    #check if they want to check validate, publish, or update enabled calls
+    read -p "
+    1. Validate? (save draft)
+    2. Publish?
+    3. Turn on?
+    Enter corresponding number or press enter to skip: " id
+    #if id not null
+    if [[ -n "$id" ]]; then
+      #if validate workflow
+      if [[ $id == 1 ]]; then
+        subquery2="log:\"workflows/validateWorkflow\""
+      #if publish workflow
+      elif [[ $id == 2 ]]; then
+        subquery2="log:\"workflows/publish\""
+      elif [[ $id == 3 ]]; then
+        subquery2="log:\"workflows/updateEnabled\""
+      fi
+      subquery="$subquery AND $subquery2"
+    fi
   #if list
   elif [[ $id_type == 4 ]]; then
     read -p "Enter list id: " id
-    subquery="log:\"listId $id\""
+    subquery="log:\"$id\""
   #if custom event
   elif [[ $id_type == 5 ]]; then
     read -p "Enter custom event name or enter to skip: " id
     #check if custom event name entered
     if [[ -n "$id" ]]; then
-      subquery="log:\"$id\" AND (log:\"events/track\" OR log:\"events/bulkTrack\")"
+      subquery="log:\"$id\" AND (log:\"events/track\" OR log:\"events/trackBulk\")"
     else
-      subquery="(log:\"events/track\" OR log:\"events/bulkTrack\")"
+      subquery="(log:\"events/track\" OR log:\"events/trackBulk\")"
     fi
   #if users/update call
   elif [[ $id_type == 6 ]]; then
-    subquery="(log:\"users/update\" OR log:\"users/updateBulk\")"
+    subquery="(log:\"users/update\" OR log:\"users/bulkUpdate\")"
   fi
 fi
+#check if subquery not null and create main query
+if [[ -n $subquery ]]; then
+  main_query="$main_query AND $subquery"
+fi
+#output query and navigate to kube
+echo "$main_query
+
+Redirecting you to kube..."
+#open kube search, must use this format or else # gets url encoded to %23
+open -n -a "Google Chrome" --args "https://logs.mon-itbl.co/app/discover#/?_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-15m,to:now))&_a=(columns:!(),filters:!(),index:'67eede50-8066-11ec-a886-f5ecefe9e33e',interval:auto,query:(language:kuery,query:'$main_query'),sort:!(!('@timestamp',desc)))"
