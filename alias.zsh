239ebea2390d0b14d95dcff8889bf25412aecdc1
estence2 <ellen.stence@iterable.com>
2022-10-24 14:19:47 -0600

    Add myprj function, rearrange functions

--- alias.zsh
+++ alias.zsh
@@ -23,18 +23,19 @@ astxt () {open -e $1}
 #Current time UTC
 utc () {date -u}
 
-#Get to my project - replace with a link to a template in your project!
+###########################################################
+#1. Get to my project - replace with a link to a template in your project!
+#myprj
 myprj () {open "https://boss.prd-itbl.co/templates/editor?templateId=7157349"
 echo "Cluster ID: 24
 Project ID: 14523
 Organization ID: 1344"}
 
-###########################################################
-#1. Hash 256 - output SHA-256 given any single input
+#2. Hash 256 - output SHA-256 given any single input
 #hash <email>
 hash () {echo -n "$1" | shasum -a 256 | awk '{print $1}'}
 
-#2. Hash your email - can optionally enter number for alias
+#3. Hash your email - can optionally enter number for alias
 #self
 self () {if [ $# -eq 0 ]; then
 #replace my email with your email
@@ -44,11 +45,11 @@ else
 echo -n "ellen.stence+$1@iterable.com" | shasum -a 256  | awk '{print $1}'
 fi}
 
-#3. boss url to app url
+#4. boss url to app url
 #b2a <boss link>
 b2a () {echo -n "$1\n" | sed 's/boss.prd-itbl.co/app.iterable.com/g'}
 
-#4. app url to boss url
+#4.1 app url to boss url
 #a2b <app link>
 a2b () {echo -n "$1\n" | sed 's/app.iterable.com/boss.prd-itbl.co/g'}
 
@@ -59,10 +60,12 @@ blobby () {bash ~/bin/blobby.zsh}
 #6. make log_<date>.txt file - take kubernetes log and output it to a more legible format,
 #create a text file and open the text file
 #log <paste log text here>
-log () {echo "$@" | sed -e 's/{/{\n/g' -e 's/}/}\n/g' -e 's/,/,\n/g' -e 's/\\//g' >> log_$(date +"%Y%m%d%H%M%S").txt
+log () {( cd "~/Desktop" &&
+echo "$@" | sed -e 's/{/{\n/g' -e 's/}/}\n/g' -e 's/,/,\n/g' -e 's/\\//g' >> log_$(date +"%Y%m%d%H%M%S").txt
 file=$(ls -t | head -n1)
 echo "New file: $file"
-open $file}
+open $file )
+}
 
 #7. get number of days away a given date is
 check_date () {
@@ -82,17 +85,17 @@ else
 fi
 }
 
-#8. write simple E2 queries
+#8. write simple ZD queries
+zd () {
+  bash ~/bin/zd.zsh
+}
+
+#9. write simple E2 queries
 e2 () {
   bash ~/bin/e2.zsh
 }
 
-#9. write simple Kubes queries
+#10. write simple Kubes queries
 kube () {
   bash ~/bin/kube.zsh
-}
-
-#10. write simple ZD queries
-zd () {
-  bash ~/bin/zd.zsh
 }
