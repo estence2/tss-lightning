62adbe5ed7513d9a4b6c0d10ba1dbf2ec5c8569e
Ellen Stence <111541880+estence2@users.noreply.github.com>
2023-08-08 18:13:03 -0600

    Correct api calls for uuid based projects

--- apiCall.zsh
+++ apiCall.zsh
@@ -3,11 +3,17 @@ api_key=$(head -1 ~/bin/apiCall.config)
 
 #id's from your project
 #replace on line 101
-campaignId=4752262
+email_campaignId=4752262
 #replace on line 105
-listId=1988320
+email_listId=1988320
 #replace on line 156
-journeyId=327907
+email_journeyId=327907
+#replace on line
+uuid_campaignId=7016862
+#replace on line
+uuid_listId=2382473
+#replace on line
+uuid_journeyId=347945
 
 #Email based or UUID based
 echo "
@@ -27,6 +33,15 @@ echo "
 4. Trigger Journey"
 read -p "Enter number of call you would like to make: " call
 echo ""
+#set unique id
+#don't need this info for campaigns/trigger
+if [[ $call != 3 ]]; then
+    if [[ $id == 2 ]]; then
+        read -p "Enter user id: " uuid
+    else
+        read -p "Enter user email: " email
+    fi
+fi
 
 endpoint=""
 fieldName="fieldName"
@@ -36,7 +51,6 @@ subquery="{"
 
 #users update call
 if [[ $call == 1 ]]; then
-    read -p "Enter user email: " email
     echo ""
     endpoint="https://api.iterable.com/api/users/update"
     #prompt for dataFields until left blank
@@ -57,15 +71,23 @@ if [[ $call == 1 ]]; then
     #only include dataFields if not null
     if [[ -z $fieldValue ]]; then
         #dataFields null
-        curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'"}'
+        #if uuid based
+        if [[ $id == 2 ]]; then
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"userId":"'${uuid}'"}'
+        else
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'"}'
+        fi
     else
         #dataFields not null 
-        curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'","dataFields":'${subquery}'}'
+        #if uuid
+        if [[ $id == 2 ]]; then
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"userId":"'${uuid}'","dataFields":'${subquery}'}'
+        else
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'","dataFields":'${subquery}'}'
+        fi
     fi
 #events/track call
 elif [[ $call == 2 ]]; then
-    read -p "Enter user email: " email
-    echo ""
     endpoint="https://api.iterable.com/api/events/track"
     read -p "Enter name of event: " eventName
     #prompt for dataFields until left blank
@@ -86,22 +108,43 @@ elif [[ $call == 2 ]]; then
     #only include dataFields if not null
     if [[ -z $fieldValue ]]; then
         #dataFields null
-        curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'","eventName":"'${eventName}'"}'
+        #if uuid based
+        if [[ $id == 2 ]]; then
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"userId":"'${uuid}'","eventName":"'${eventName}'"}'
+        else
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'","eventName":"'${eventName}'"}'
+        fi
     else
         #dataFields not null 
-        curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'","eventName":"'${eventName}'","dataFields":'${subquery}'}'
+        #if uuid
+        if [[ $id == 2 ]]; then
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"userId":"'${uuid}'","eventName":"'${eventName}'","dataFields":'${subquery}'}'
+        else
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'","eventName":"'${eventName}'","dataFields":'${subquery}'}'
+        fi
     fi
 #trigger campaign
 elif [[ $call == 3 ]]; then
     endpoint="https://api.iterable.com/api/campaigns/trigger"
     #set campaign id
-    read -p "Enter campaign id. If empty, default is '${campaignId}': " campaignId
-    #if campaignId blank, set default value
-    campaignId=${campaignId:-4752262}
-    #set send list id
-    read -p "Enter list id. If empty, default is '${listId}': " listId
-    #if listId blank, set default value
-    listId=${listId:-1988320}
+    #uuid project
+    if [[ $id == 2 ]]; then
+        read -p "Enter campaign id. If empty, default is '${uuid_campaignId}': " campaignId
+        #if campaignId blank, set default value
+        campaignId=${campaignId:-7016862}
+        #set send list id
+        read -p "Enter list id. If empty, default is '${uuid_listId}': " listId
+        #if listId blank, set default value
+        listId=${listId:-2382473}
+    else
+        read -p "Enter campaign id. If empty, default is '${email_campaignId}': " campaignId
+        #if campaignId blank, set default value
+        campaignId=${campaignId:-4752262}
+        #set send list id
+        read -p "Enter list id. If empty, default is '${email_listId}': " listId
+        #if listId blank, set default value
+        listId=${listId:-1988320}
+    fi
     #add brackets to make list
     listId=[${listId}]
     #set suppression list id
@@ -148,11 +191,14 @@ elif [[ $call == 3 ]]; then
     fi
 #trigger workflow
 elif [[ $call == 4 ]]; then
-    read -p "Enter user email: " email
-    echo ""
     endpoint="https://api.iterable.com/api/workflows/triggerWorkflow"
-    read -p "Enter journey id. If empty, default is '${journeyId}': "
-    journeyId=${journeyId:-327907}
+    if [[ $id == 2 ]]; then
+        read -p "Enter journey id. If empty, default is '${uuid_journeyId}': " journeyId
+        journeyId=${journeyId:-347945}
+    else
+        read -p "Enter journey id. If empty, default is '${email_journeyId}': " journeyId
+        journeyId=${journeyId:-327907}
+    fi    
     echo ""
     #prompt for dataFields until left blank
     until [[ -z $fieldName ]]
@@ -172,9 +218,19 @@ elif [[ $call == 4 ]]; then
     #only include dataFields if not null
     if [[ -z $fieldValue ]]; then
         #dataFields null
-        curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'","workflowId":'${journeyId}'}'
+        #if uuid based
+        if [[ $id == 2 ]]; then
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"userId":"'${uuid}'","workflowId":'${journeyId}'}'
+        else
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'","workflowId":'${journeyId}'}'
+        fi
     else
-        #dataFields not null
-        curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'","workflowId":'${journeyId}',"dataFields":'${subquery}'}'
+        #dataFields not null 
+        #if uuid
+        if [[ $id == 2 ]]; then
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"userId":"'${uuid}'","workflowId":'${journeyId}',"dataFields":'${subquery}'}'
+        else
+            curl -XPOST "${endpoint}" -H "Api-Key: ${api_key}" -d '{"email":"'${email}'","workflowId":'${journeyId}',"dataFields":'${subquery}'}'
+        fi
     fi
 fi
